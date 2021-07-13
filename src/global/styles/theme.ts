d54f7360c7d0e8aaa162be0e0a190caec93ef473
Anderson Felipe <anderson_fpv@yahoo.com.br>
2021-07-13 12:56:22 -0300

    Utilização de fontes personalizadas

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -14,6 +14,12 @@ export default {
         shape: '#FFFFFF',
         title: '#363F5F',
         text: '#969CB2',
-        background: '##F0F2F5',
-    }
+        background: '#F0F2F5',
+    },
+
+    fonts: {
+        regular: 'Poppins_400Regular',
+        medium: 'Poppins_500Medium',
+        bold: 'Poppins_700Bold',
+    },
 }
