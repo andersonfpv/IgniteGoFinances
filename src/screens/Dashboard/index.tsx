f3736492b4bfe369c9b94bf288efc115adf52281
Anderson Felipe <anderson_fpv@yahoo.com.br>
2021-07-13 20:31:20 -0300

    cabeçalho da aplicação com o auxílio da biblioteca react-native-responsive-fontsize

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  UserWrapper,
+} from "./styles";
 
 export function Dashboard(){
     return (
-        <Container>
-            <Title>Dashboard</Title>
-        </Container>
-    )
+      <Container>
+        <Header>
+            <UserWrapper>
+                <UserInfo>
+                    <Photo source={{ uri: "https://github.com/andersonfpv.png" }} />
+                    <User>
+                    <UserGreeting>Olá</UserGreeting>
+                    <UserName>Anderson</UserName>
+                    </User>
+                </UserInfo>
+            </UserWrapper>
+        </Header>
+      </Container>
+    );
 }
