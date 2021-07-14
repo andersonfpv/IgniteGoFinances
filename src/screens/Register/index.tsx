de59c9f4510c855d7813a4f7ca74318520fcc514
Anderson Felipe <anderson_fpv@yahoo.com.br>
2021-07-14 16:26:58 -0300

    correção no espaçamento entre o componente TransactionsTypes e os Inputs e desenvolvimento do componente de Select para selecionar as categorias do cadastro

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Input } from '../../components/Form/Input';
 import { Button } from '../../components/Form/Button';
 import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
+import { CategorySelect } from '../../components/Form/CategorySelect';
 import { 
     Container,
     Header,
@@ -48,6 +49,7 @@ export function Register() {
                         />
 
                     </TransactionsTypes>
+                    <CategorySelect title="Categoria"/>
                 </Fields>
 
             <Button title="Enviar"/>
