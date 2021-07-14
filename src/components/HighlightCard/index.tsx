05b7ed91120eed6ab5e484583b6096e91abeb0be
Anderson Felipe <anderson_fpv@yahoo.com.br>
2021-07-13 23:40:51 -0300

    dando continuidade no desenvolvimento dos cartões. Tipando propriedades no Styled Components e tornando o componente HighlightCard mais dinâmico

--- src/components/HighlightCard/index.tsx
+++ src/components/HighlightCard/index.tsx
@@ -10,17 +10,44 @@ import {
     LastTransaction,
 } from './styles';
 
-export function HighlightCard(){
-   return(
-    <Container>
-            <Header>
-                <Title>Entrada</Title>
-                <Icon name="arrow-up-circle"/>
-            </Header>
-            <Footer>
-                <Amount>17.400,00</Amount>
-                <LastTransaction>Última entrada dia 13 de abril</LastTransaction>
-            </Footer>
-        </Container>
-   )
+interface Props {
+    title: string;
+    amount: string;
+    lastTransaction: string;
+    type: 'up' | 'down' | 'total';
+}
+
+const icon = {
+    up: 'arrow-up-circle',
+    down: 'arrow-down-circle',
+    total: 'dollar-sign',
+}
+
+export function HighlightCard({
+    title, 
+    amount, 
+    lastTransaction,
+    type,
+} : Props){
+   return (
+     <Container type={type}>
+       <Header>
+         <Title type={type}>
+            {title}
+        </Title>
+         <Icon 
+            name={icon[type]} 
+            type={type} 
+        />
+       </Header>
+       <Footer>
+         <Amount type={type}>
+            {amount}
+        </Amount>
+         <LastTransaction type={type}>
+            {lastTransaction}
+        </LastTransaction>
+       </Footer>
+     </Container>
+   );
 }
