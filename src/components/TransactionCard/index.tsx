d948c66829436c57ee21b09d94e7e1f1dc896c8b
Anderson Felipe <anderson_fpv@yahoo.com.br>
2021-07-14 01:38:12 -0300

    continuidade na construção da lista de transações, refatoração do componente de TransactionCard para torná-lo mais dinâmico

--- src/components/TransactionCard/index.tsx
+++ src/components/TransactionCard/index.tsx
@@ -11,19 +11,46 @@ import {
     Date,
 } from './styles';
 
-export function TransactionCard(){
-    return (
-        <Container>
-            <Title>Desenvolvimento de site</Title>
-            <Amount>R$ 12.000,00</Amount>
+interface Category {
+    name: string;
+    icon: string;
+}
 
-            <Footer>
-                <Category>
-                    <Icon name="dollar-sign"/>
-                    <CategoryName>Vendas</CategoryName>
-                </Category>
-                <Date>13/04/2020</Date>
-            </Footer>
-        </Container>
-    )
+interface Data {
+  title: string;
+  amount: string;
+  category: Category;
+  date: string;
+}
+
+interface Props {
+    data: Data;
+}
+
+export function TransactionCard({ data } : Props){
+    
+    return (
+      <Container>
+        <Title>
+            {data.title
+        }</Title>
+
+        <Amount>
+            {data.amount}
+        </Amount>
+
+        <Footer>
+          <Category>
+            <Icon name="dollar-sign" />
+            <CategoryName>
+                {data.category.name}
+            </CategoryName>
+          </Category>
+
+          <Date>
+            {data.date}
+          </Date>
+        </Footer>
+      </Container>
+    );
 }
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 import { HighlightCard } from '../../components/HighlightCard';
 import { TransactionCard } from '../../components/TransactionCard';
 import {
@@ -14,50 +15,86 @@ import {
   HighlightCards,
   Transactions,
   Title,
+  TransactionList,
 } from "./styles";
 
 export function Dashboard(){
-    return (
-      <Container>
-        <Header>
-          <UserWrapper>
-            <UserInfo>
-              <Photo source={{ uri: "https://github.com/andersonfpv.png" }} />
-              <User>
-                <UserGreeting>Olá</UserGreeting>
-                <UserName>Anderson</UserName>
-              </User>
-            </UserInfo>
-            <Icon name="power" />
-          </UserWrapper>
-        </Header>
+  const data = [
+    {
+      title: "Desenvolvimento de site",
+      amount: "R$ 12.000,00",
+      category: {
+        name: "Vendas",
+        icon: "dollar-sign",
+      },
+      date: "13/04/2020",
+    },
+    {
+      title: "Desenvolvimento de site",
+      amount: "R$ 12.000,00",
+      category: {
+        name: "Vendas",
+        icon: "dollar-sign",
+      },
+      date: "13/04/2020",
+    },
+    {
+      title: "Desenvolvimento de site",
+      amount: "R$ 12.000,00",
+      category: {
+        name: "Vendas",
+        icon: "dollar-sign",
+      },
+      date: "13/04/2020",
+    },
+];
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: "https://github.com/andersonfpv.png" }} />
+            <User>
+              <UserGreeting>Olá</UserGreeting>
+              <UserName>Anderson</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
 
-        <HighlightCards>
-          <HighlightCard
-            type="up"
-            title="Entradas"
-            amount="R$ 17.400,00"
-            lastTransaction="Última entrada dia 13 de abril"
-          />
-          <HighlightCard
-            type="down"
-            title="Saídas"
-            amount="R$ 1.259,00"
-            lastTransaction="Última saída dia 03 de abril"
-          />
-          <HighlightCard
-            type="total"
-            title="Total"
-            amount="R$ 16.141,00"
-            lastTransaction="01 à 16 de abril"
-          />
-        </HighlightCards>
+      <HighlightCards>
+        <HighlightCard
+          type="up"
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HighlightCard
+          type="down"
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última saída dia 03 de abril"
+        />
+        <HighlightCard
+          type="total"
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransaction="01 à 16 de abril"
+        />
+      </HighlightCards>
 
-        <Transactions>
-          <Title>Listagem</Title>
-
-          <TransactionCard />
-        </Transactions>
-      </Container>
-    );
+      <Transactions>
+        <Title>Listagem</Title>
+        <TransactionList 
+          data={data}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{
+            paddingBottom: getBottomSpace()
+          }}
+        />
+      </Transactions>
+    </Container>
+  );
 }
