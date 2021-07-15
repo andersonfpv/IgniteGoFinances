89ae07b2ff45eca0f5bbddbc2b4c188496187a8d
Anderson Felipe <anderson_fpv@yahoo.com.br>
2021-07-14 23:36:16 -0300

    refactor no CategorySelect do Form para CategorySelectButton evitando possíveis conflitos com o CategorySelect da pasta screens. Criação domodal com o componente Modal do React Native e criação de botões com o GestureHandlerRootView da biblioteca react-native-gesture-handler para uma melhor solução entre dispositivos

--- src/screens/CategorySelect/index.tsx
+++ src/screens/CategorySelect/index.tsx
@@ -5,55 +5,62 @@ import { Button } from "../../components/Form/Button";
 
 import { categories } from "../../utils/categories";
 
-import { 
-    Container,
-    Header,
-    Title,
-    Category,
-    Icon,
-    Name,
-    Separator,
-    Footer, 
+import {
+  Container,
+  Header,
+  Title,
+  Category,
+  Icon,
+  Name,
+  Separator,
+  Footer,
 } from "./styles";
 
 interface Category {
-    key: string;
-    name: string;
+  key: string;
+  name: string;
 }
 
 interface Props {
-    category: string;
-    setCategory: (category: Category) => void;
-    closeSelectCategory: () => void;
+  category: Category;
+  setCategory: (category: Category) => void;
+  closeSelectCategory: () => void;
 }
 
 export function CategorySelect({
-    category,
-    setCategory,
-    closeSelectCategory
-}: Props){
-    return(
-        <Container>
-            <Header>
-                <Title>Categoria</Title>
-            </Header>
+  category,
+  setCategory,
+  closeSelectCategory,
+}: Props) {
+  function handleCategorySelect(category: Category) {
+    setCategory(category);
+  }
 
-            <FlatList 
-                data={categories}
-                style={{ flex: 1, width: '100%' }}
-                keyExtractor={(item) => item.key}
-                renderItem={({ item }) => (
-                    <Category>
-                        <Icon name={item.icon} />
-                        <Name>{item.name}</Name>
-                    </Category>
-                )}
-                ItemSeparatorComponent={() => <Separator />}
-            />
+  return (
+    <Container>
+      <Header>
+        <Title>Categoria</Title>
+      </Header>
 
-            <Footer>
-                <Button title="Selecionar" />
-            </Footer>
-        </Container>
-    )
+      <FlatList
+        data={categories}
+        style={{ flex: 1, width: "100%" }}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => (
+          <Category
+            onPress={() => handleCategorySelect(item)}
+            isActive={category.key === item.key}
+          >
+            <Icon name={item.icon} />
+            <Name>{item.name}</Name>
+          </Category>
+        )}
+        ItemSeparatorComponent={() => <Separator />}
+      />
+
+      <Footer>
+        <Button title="Selecionar" onPress={closeSelectCategory} />
+      </Footer>
+    </Container>
+  );
 }
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
+import { Modal } from 'react-native';
 import { Input } from '../../components/Form/Input';
 import { Button } from '../../components/Form/Button';
 import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
-import { CategorySelect } from '../../components/Form/CategorySelect';
+import { CategorySelectButton } from '../../components/Form/CategorySelectButton';
+
+import { CategorySelect } from '../CategorySelect';
+
 import { 
     Container,
     Header,
@@ -15,45 +19,65 @@ import {
 export function Register() {
 
     const [transactionType, setTransactionType] = useState('');
+    const [categoryModalOpen, setCategoryModalOpen] = useState(false);
+    
+    const [category, setCategory] = useState({
+        key: 'category',
+        name: 'Categoria',
+    });
 
     function handleTransactionsTypeSelect(type: 'up' | 'down'){
         setTransactionType(type);
     }
 
-    return(
-        <Container>
-            <Header>
-                <Title>Cadastro</Title>
-            </Header>
+    function handleOpenSelectCategoryModal() {
+        setCategoryModalOpen(true);
+    }
 
-            <Form>
-                <Fields>
-                    <Input 
-                        placeholder="Nome"
-                    />
-                    <Input 
-                        placeholder="Preço"
-                    />
-                    <TransactionsTypes>
-                        <TransactionTypeButton 
-                            type="up"
-                            title="Income"
-                            onPress={() => handleTransactionsTypeSelect('up')}
-                            isActive={transactionType === 'up'}
-                        />
-                        <TransactionTypeButton 
-                            type="down"
-                            title="Outcome"
-                            onPress={() => handleTransactionsTypeSelect('down')}
-                            isActive={transactionType === 'down'}
-                        />
+    function handleCloseSelectCategoryModal() {
+        setCategoryModalOpen(false);
+    }
 
-                    </TransactionsTypes>
-                    <CategorySelect title="Categoria"/>
-                </Fields>
+    return (
+      <Container>
+        <Header>
+          <Title>Cadastro</Title>
+        </Header>
 
-            <Button title="Enviar"/>
-            </Form>
-        </Container>
+        <Form>
+          <Fields>
+            <Input placeholder="Nome" />
+            <Input placeholder="Preço" />
+            <TransactionsTypes>
+              <TransactionTypeButton
+                type="up"
+                title="Income"
+                onPress={() => handleTransactionsTypeSelect("up")}
+                isActive={transactionType === "up"}
+              />
+              <TransactionTypeButton
+                type="down"
+                title="Outcome"
+                onPress={() => handleTransactionsTypeSelect("down")}
+                isActive={transactionType === "down"}
+              />
+            </TransactionsTypes>
+            <CategorySelectButton 
+                title={category.name}
+                onPress={handleOpenSelectCategoryModal} 
+            />
+          </Fields>
+
+          <Button title="Enviar" />
+        </Form>
+
+        <Modal visible={categoryModalOpen}>
+          <CategorySelect
+            category={category}
+            setCategory={setCategory}
+            closeSelectCategory={handleCloseSelectCategoryModal}
+          />
+        </Modal>
+      </Container>
     );
 }
