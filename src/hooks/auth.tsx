ddd57437242a62d6e645525f811adde2e8a0aade
Anderson Felipe <anderson_fpv@yahoo.com.br>
2021-07-18 18:04:13 -0300

    Conectando com uma conta Google

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,9 +1,11 @@
 import React, { 
     createContext,
     ReactNode,
-    useContext 
-} from "react";
-import { UseFormReset } from "react-hook-form";
+    useContext,
+    useState 
+} from 'react';
+
+import * as AuthSession from 'expo-auth-session';
 
 
 interface AuthProviderProps {
@@ -13,29 +15,69 @@ interface AuthProviderProps {
 interface User {
     id: string;
     name: string;
-    emai: string;
+    email: string;
     photo?: string;
 }
 
 interface IAuthContextData {
     user: User;
+    signInWithGoogle(): Promise<void>;
+}
+
+interface AuthorizationResponse {
+    params: {
+        acess_token: string;
+    }, 
+    type: string;
 }
 
 const AuthContext = createContext({} as IAuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
-    const user = {
-        id: '1234',
-        name: 'Anderson Felipe',
-        email: 'anderson.ti.senac@gmail.com',
-    };
+    const [user, setUser] = useState<User>({} as User);
+
+    async function signInWithGoogle() {
+        try {
+            const CLIENT_ID ="88265890666-mr0pupevf8mvg31esckeet5t05q56g23.apps.googleusercontent.com";
+            const REDIRECT_URI = "https://auth.expo.io/@and_fpv/gofinances";
+            const RESPONSE_TYPE = 'token';
+            const SCOPE = encodeURI('profile email');
+    
+            const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+    
+            const { type, params } = await AuthSession.startAsync({ authUrl }) as AuthorizationResponse;
+            
+            if(type === 'success'){
+                const response = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`);
+
+                const userInfo = await response.json();
+
+                setUser({
+                    id: userInfo.id,
+                    email: userInfo.email,
+                    name: userInfo.given_name,
+                    photo: userInfo.picture
+                });
+               
+            }
+            
+            
+    
+        } catch ( error ) {
+            throw new Error(error);
+        }
+    }
 
     return(
-        <AuthContext.Provider value={{ user }}>
+        <AuthContext.Provider value={{ 
+            user, 
+            signInWithGoogle 
+        }}>
             { children }
         </AuthContext.Provider>
     )
 }
+
 
 function useAuth(){
     const context = useContext(AuthContext);
@@ -43,4 +85,4 @@ function useAuth(){
     return context;
 }
 
-export { AuthProvider, useAuth };
+export { AuthProvider, useAuth }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Alert } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 import AppleSvg from '../../assets/apple.svg';
@@ -20,41 +21,44 @@ import {
 } from "./styles";
 
 export function SignIn(){
-    const { user } = useAuth();
-    console.log(user);
-    return(
-        <Container>
-            <Header>
-                <TitleWrapper>
-                    <LogoSvg 
-                        width={RFValue(120)}
-                        height={RFValue(68)}
-                    />
-                    <Title>
-                        Controle suas {'\n'}
-                        finanças de forma {'\n'}
-                        muito simples    
-                    </Title>
-                </TitleWrapper>
+    const { signInWithGoogle } = useAuth();
+    
+    async function handleSignInWithGoogle(){
+        try{
+            await signInWithGoogle();
+        }catch (error){
+            console.log(error);
+            Alert.alert('Não foi possível conectar a conta Google')
+        }
+    }
+    return (
+      <Container>
+        <Header>
+          <TitleWrapper>
+            <LogoSvg width={RFValue(120)} height={RFValue(68)} />
+            <Title>
+              Controle suas {"\n"}
+              finanças de forma {"\n"}
+              muito simples
+            </Title>
+          </TitleWrapper>
 
-                <SignInTitle>
-                    Faça seu login com {'\n'}
-                    uma das contas abaixo 
-                </SignInTitle>
-            </Header>
+          <SignInTitle>
+            Faça seu login com {"\n"}
+            uma das contas abaixo
+          </SignInTitle>
+        </Header>
 
-            <Footer>
-                <FooterWrapper>
-                    <SignInSocialButton 
-                        title="Entrar com Google"
-                        svg={GoogleSvg}
-                    />
-                    <SignInSocialButton 
-                        title="Entra com Apple"
-                        svg={AppleSvg}
-                    />
-                </FooterWrapper>
-            </Footer>
-        </Container>
+        <Footer>
+          <FooterWrapper>
+            <SignInSocialButton
+              title="Entrar com Google"
+              svg={GoogleSvg}
+              onPress={handleSignInWithGoogle}
+            />
+            <SignInSocialButton title="Entra com Apple" svg={AppleSvg} />
+          </FooterWrapper>
+        </Footer>
+      </Container>
     );
 }
